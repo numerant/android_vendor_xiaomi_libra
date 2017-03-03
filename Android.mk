8387d2c1a9e555033ab5abb55611fc45c86dd0ce
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-03-03 22:17:53 +0200

    Revert "Flag com.qualcomm.location as presigned"
    This reverts commit 3a9122c95e85313f8fd244d6c8d448aa1d468c08.

--- Android.mk
+++ Android.mk
@@ -218,7 +218,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
 LOCAL_MODULE_OWNER := xiaomi
 LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
